226d548fe14ecaa461683a83c9e44628e51d8d65
Juan Lüthy <juan.luethy@gmail.com>
2025-04-04 11:04:21 +0200

    Big Bonk :thumbsup:

---
+++ src/main/java/ch/kuan/timestamp/TimestampApplication.java
@@ -0,0 +1,15 @@
+package ch.kuan.timestamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class TimestampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimestampApplication.class, args);
+	}
+
+}
